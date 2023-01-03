c564118d1d2d0e08b6918cf5b3b562ae64e59a76
Tiago Jose <tiago_jvo@outlook.com>
2023-01-03 20:45:08 -0300

    feat: add email input validator and state handler

---
+++ script.js
@@ -0,0 +1,21 @@
+const emailInput = document.querySelector('input[type="email"]')
+
+const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+}
+
+
+const handleEmailBlur = ({ target : input}) => {
+    const { value : email } = input
+
+    const isValidEmail = validateEmail(email)
+
+    if(!isValidEmail){
+        emailInput.setAttribute('aria-invalid', true)
+    }else {
+        emailInput.setAttribute('aria-invalid', false)
+    }
+}
+
+emailInput.addEventListener('blur', handleEmailBlur)
